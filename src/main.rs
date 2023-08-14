94f3c0ecb4491e6b84d037b2d38900ed8c04691b
Russel Arbore <russel.jma@gmail.com>
2023-08-13 18:45:45 -0700

    Refactor metadata scraping

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
-extern crate wikipedia;
-use scraper::Html;
+mod metadata;
 
-fn main() {
+fn main() -> Result<(), &'static str> {
     let mut args: Vec<String> = std::env::args().collect();
     assert!(
         args.len() > 1,
@@ -17,143 +16,8 @@ fn main() {
         })
         .collect();
 
-    let wiki = wikipedia::Wikipedia::<wikipedia::http::default::Client>::default();
-    let page = wiki.page_from_title(album_name.clone());
-    let content = page
-        .get_html_content()
-        .expect(&("Couldn't find wikipedia page about \"".to_owned() + &album_name + "\"."));
-    let document = Html::parse_document(&content);
-    let mut track_listings = vec![];
-    let mut author = None;
-    let mut album_cover = None;
+    let metadata = metadata::scrape_wikipedia(&album_name, &listings_to_use)?;
+    println!("{}", metadata);
 
-    for node in document.tree.nodes() {
-        let value = node.value();
-        if value.is_element()
-            && value.as_element().unwrap().has_class(
-                "contributor",
-                scraper::CaseSensitivity::AsciiCaseInsensitive,
-            )
-            && author == None
-        {
-            author = Some(
-                node.first_child()
-                    .unwrap()
-                    .value()
-                    .as_element()
-                    .unwrap()
-                    .attr("title")
-                    .unwrap()
-                    .replace(" (band)", ""),
-            );
-        } else if value.is_element()
-            && value.as_element().unwrap().has_class(
-                "infobox-image",
-                scraper::CaseSensitivity::AsciiCaseInsensitive,
-            )
-            && album_cover == None
-        {
-            for node in node.descendants() {
-                let value = node.value();
-                if value.is_element()
-                    && value.as_element().unwrap().has_class(
-                        "mw-file-description",
-                        scraper::CaseSensitivity::AsciiCaseInsensitive,
-                    )
-                {
-                    album_cover = Some(value.as_element().unwrap().attr("href").unwrap());
-                }
-            }
-        } else if value.is_element()
-            && value.as_element().unwrap().has_class(
-                "track-listing",
-                scraper::CaseSensitivity::AsciiCaseInsensitive,
-            )
-        {
-            let mut tracks = vec![];
-            'tr: for node in node.descendants() {
-                let value = node.value();
-                if value.is_element() && value.as_element().unwrap().name() == "tr" {
-                    for node in node.descendants() {
-                        let value = node.value();
-                        if value.is_element() && value.as_element().unwrap().name() == "td" {
-                            for node in node.descendants() {
-                                if node.value().is_text() {
-                                    let mut base =
-                                        String::from(node.value().as_text().unwrap() as &str);
-                                    for node in node.next_siblings() {
-                                        base += match node.value() {
-                                            scraper::node::Node::Element(elem) => {
-                                                elem.attr("title").unwrap()
-                                            }
-                                            scraper::node::Node::Text(text) => text as &str,
-                                            _ => "",
-                                        };
-                                    }
-                                    tracks.push(base);
-                                    continue 'tr;
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-            tracks.pop().unwrap();
-            track_listings.push(tracks);
-        }
-    }
-
-    println!(
-        "Here are the tracks found for the album \"{}\", authored by \"{}\":\n[",
-        page.get_title().unwrap(),
-        author.clone().unwrap_or(String::from("Unknown"))
-    );
-    for tracks in track_listings.iter() {
-        println!("  [");
-        for track in tracks {
-            println!("    {}", track);
-        }
-        println!("  ]");
-    }
-    if listings_to_use.len() == 0 && track_listings.len() > 1 {
-        println!("]\nSince there is more than one track listing, an explicit specification of which listings should be included in the download is required. Please specify at least one item from the list to download. List the listing number of each listing to include, in order (zero indexed).");
-        return;
-    }
-
-    let mut tracklist = vec![];
-    for listing in listings_to_use {
-        for track in track_listings[listing].iter() {
-            let mut trackname = String::from(track.split("\"").nth(1).unwrap());
-            let mut remove_patterns = vec![String::from(" (song)")];
-            if let Some(author) = &author {
-                remove_patterns.push(String::from(" (") + author + " song)");
-                remove_patterns.push(String::from(" (") + author + " EP)");
-            }
-            for pattern in remove_patterns {
-                trackname = trackname.replace(&pattern, "");
-            }
-            tracklist.push(trackname);
-        }
-    }
-
-    let mut cover_url = None;
-    if let Some(album_cover) = album_cover {
-        let cover_name = album_cover.split("File:").nth(1).unwrap();
-        for image in page.get_images().unwrap() {
-            if image.url.contains(cover_name) {
-                cover_url = Some(image.url);
-            }
-        }
-    }
-    let mut cover_image = None;
-    if let Some(cover_url) = cover_url {
-        println!("Here's the URL to the album cover: {}", cover_url);
-        let cover = reqwest::blocking::get(cover_url).unwrap().bytes().unwrap();
-        let cover = image::load_from_memory(&cover).unwrap();
-        cover_image = Some(cover);
-    }
-
-    for track in tracklist {
-        println!("{}", track);
-    }
+    Ok(())
 }
---
+++ src/metadata.rs
@@ -0,0 +1,160 @@
+extern crate wikipedia;
+use scraper::Html;
+
+pub struct Album {
+    name: String,
+    track_names: Vec<String>,
+    cover: Option<image::DynamicImage>,
+}
+
+impl std::fmt::Display for Album {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> Result<(), std::fmt::Error> {
+        write!(f, "Album Name: {}\n\n", self.name)?;
+        write!(f, "Track Listing:\n")?;
+        for (idx, name) in self.track_names.iter().enumerate() {
+            write!(f, "{}: {}\n", idx, name)?;
+        }
+        if let Some(cover) = &self.cover {
+            write!(
+                f,
+                "\nCover Dimensions: {} x {}\n",
+                cover.width(),
+                cover.height()
+            )?;
+        }
+        Ok(())
+    }
+}
+
+pub fn scrape_wikipedia(
+    album_name: &str,
+    track_list_nums: &[usize],
+) -> Result<Album, &'static str> {
+    let wiki = wikipedia::Wikipedia::<wikipedia::http::default::Client>::default();
+    let page = wiki.page_from_title(album_name.to_string());
+    let content = page
+        .get_html_content()
+        .expect(&("Couldn't find wikipedia page about \"".to_owned() + &album_name + "\"."));
+    let document = Html::parse_document(&content);
+    let mut track_listings: Vec<Vec<String>> = vec![];
+    let mut author = None;
+    let mut album_cover = None;
+
+    for node in document.tree.nodes() {
+        let value = node.value();
+        if value.is_element()
+            && value.as_element().unwrap().has_class(
+                "contributor",
+                scraper::CaseSensitivity::AsciiCaseInsensitive,
+            )
+            && author == None
+        {
+            author = Some(
+                node.first_child()
+                    .unwrap()
+                    .value()
+                    .as_element()
+                    .unwrap()
+                    .attr("title")
+                    .unwrap()
+                    .replace(" (band)", ""),
+            );
+        } else if value.is_element()
+            && value.as_element().unwrap().has_class(
+                "infobox-image",
+                scraper::CaseSensitivity::AsciiCaseInsensitive,
+            )
+            && album_cover == None
+        {
+            for node in node.descendants() {
+                let value = node.value();
+                if value.is_element()
+                    && value.as_element().unwrap().has_class(
+                        "mw-file-description",
+                        scraper::CaseSensitivity::AsciiCaseInsensitive,
+                    )
+                {
+                    album_cover = Some(value.as_element().unwrap().attr("href").unwrap());
+                }
+            }
+        } else if value.is_element()
+            && value.as_element().unwrap().has_class(
+                "track-listing",
+                scraper::CaseSensitivity::AsciiCaseInsensitive,
+            )
+        {
+            let mut tracks = vec![];
+            'tr: for node in node.descendants() {
+                let value = node.value();
+                if value.is_element() && value.as_element().unwrap().name() == "tr" {
+                    for node in node.descendants() {
+                        let value = node.value();
+                        if value.is_element() && value.as_element().unwrap().name() == "td" {
+                            for node in node.descendants() {
+                                if node.value().is_text() {
+                                    let mut base =
+                                        String::from(node.value().as_text().unwrap() as &str);
+                                    for node in node.next_siblings() {
+                                        base += match node.value() {
+                                            scraper::node::Node::Element(elem) => {
+                                                elem.attr("title").unwrap()
+                                            }
+                                            scraper::node::Node::Text(text) => text as &str,
+                                            _ => "",
+                                        };
+                                    }
+                                    tracks.push(base);
+                                    continue 'tr;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            tracks.pop().unwrap();
+            track_listings.push(tracks);
+        }
+    }
+
+    if track_list_nums.len() == 0 && track_listings.len() > 1 {
+        return Err("Since there is more than one track listing, an explicit specification of which listings should be included in the download is required. Please specify at least one item from the list to download. List the listing number of each listing to include, in order (zero indexed).");
+    }
+
+    let mut tracklist = vec![];
+    for listing in track_list_nums {
+        for track in track_listings[*listing].iter() {
+            let mut trackname = String::from(track.split("\"").nth(1).unwrap());
+            let mut remove_patterns = vec![String::from(" (song)")];
+            if let Some(author) = &author {
+                remove_patterns.push(String::from(" (") + author + " song)");
+                remove_patterns.push(String::from(" (") + author + " EP)");
+            }
+            for pattern in remove_patterns {
+                trackname = trackname.replace(&pattern, "");
+            }
+            tracklist.push(trackname);
+        }
+    }
+
+    let mut cover_url = None;
+    if let Some(album_cover) = album_cover {
+        let cover_name = album_cover.split("File:").nth(1).unwrap();
+        for image in page.get_images().unwrap() {
+            if image.url.contains(cover_name) {
+                cover_url = Some(image.url);
+            }
+        }
+    }
+    let mut cover_image = None;
+    if let Some(cover_url) = cover_url {
+        let cover = reqwest::blocking::get(cover_url).unwrap().bytes().unwrap();
+        let cover = image::load_from_memory(&cover).unwrap();
+        cover_image = Some(cover);
+    }
+
+    Ok(Album {
+        name: album_name.to_string(),
+        track_names: tracklist,
+        cover: cover_image,
+    })
+}
